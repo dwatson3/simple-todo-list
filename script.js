e53bc2665f9614ec4f04127baffd4ff796ad30d3
Daphne Watson <daphne_watson@Daphnes-MacBook-Air.local>
2015-05-19 11:35:00 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var btnNew = document.querySelector("button");
+var listItem = document.getElementById("input");
+
+// var userInput = 
+
+btnNew.addEventListener("click", function(e) {
+	// var newInput = document.getElementById("add_todo");
+	var newValue = input.value;
+	e.preventDefault();
+	alert(newValue);
+	
+	// listItem.appendChild(newValue);
+});
+
+
+listItem.addEventListener("click", function(e) {
+	var newInput = input.value;
+	e.preventDefault();
+	alert(newInput);
+
+});
+
+
+
+
+
+
